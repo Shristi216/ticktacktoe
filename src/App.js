befc3f772d2932709910dac29a1b34e0279296ac
Shristi216 <guptass_7@rknec.edu>
2022-09-21 16:39:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,89 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
-import logo from './logo.svg';
+import SquareComponent from './SquareComponent';
 import './App.css';
 import particlesOptions from "./particles.json";
-
+const initialState = ["", "", "", "", "", "", "", "", ""];
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+  const [gameState, updateGameState] = useState(initialState);
+  const [isXChance, updateIsXChance] = useState(false);
+  const onSquareClicked = (index) => {
+    let strings = Array.from(gameState);
 
-    return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+    //  <FaHeart style={{color: '	#FF69B4', fontSize: '50px'}}/> : <FaRegHeart 	style={{color: '	#FF69B4', fontSize: '50px'}}/>
+    strings[index] = isXChance ? "X" : "O";
+    updateGameState(strings);
+    updateIsXChance(!isXChance);
+  }
+  useEffect(() => {
+
+    const winner = checkWinner();
+
+    if (winner) {
+      < SquareComponent className="b-bottom-right" state={gameState[winner]} onClick={() => onSquareClicked(winner)} />
+      alert(`YAY! ${winner} has won the Game!`)
+      updateGameState(initialState)
+    }
+  }, [gameState])
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        return gameState[a];
+      }
+    }
+    return null;
+  }
+  const particlesInit = useCallback(main => {
+    loadFull(main);
+  }, [])
+
+  return (
+    <div className="App">
+      <Particles options={particlesOptions} init={particlesInit} />
+      <header className="App-header">
+        <p className="heading-text"> TIC TAC TOE </p>
+        <div className="basic">
+          <div className="row jc-center">
+            < SquareComponent className="b-bottom-right" state={gameState[0]} onClick={() => onSquareClicked(0)} />
+            < SquareComponent className="b-bottom-right" state={gameState[1]} onClick={() => onSquareClicked(1)} />
+            < SquareComponent className="b-bottom" state={gameState[2]} onClick={() => onSquareClicked(2)} />
+          </div>
         </div>
-    );
+        <div className="basic">
+          <div className="row jc-center">
+
+            < SquareComponent className="b-bottom-right" state={gameState[3]} onClick={() => onSquareClicked(3)} />
+            < SquareComponent className="b-bottom-right" state={gameState[4]} onClick={() => onSquareClicked(4)} />
+            < SquareComponent className="b-bottom" state={gameState[5]} onClick={() => onSquareClicked(5)} />
+          </div>
+        </div>
+        <div className="basic">
+          <div className="row jc-center">
+            < SquareComponent className="b-right " state={gameState[6]} onClick={() => onSquareClicked(6)} />
+            < SquareComponent className="b-right" state={gameState[7]} onClick={() => onSquareClicked(7)} />
+            < SquareComponent state={gameState[8]} onClick={() => onSquareClicked(8)} />
+          </div>
+        </div>
+        <button className="clear-button" onClick={() => updateGameState(initialState)}><div className="nameb">NEW GAME</div></button>
+
+
+
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
+// 👇️ IMPORTANT: use correct ID of your root element
+// this is the ID of the div in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
